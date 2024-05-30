7c2241bf6e578838fbe8ab9f7351482140fba6d1
Dane Falberg <70166906+pricelowtohigh@users.noreply.github.com>
2024-05-30 13:59:10 -0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Document, Page, Text, View, StyleSheet, PDFViewer } from "@react-pdf/renderer";
 
-function App() {
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "#d11fb6",
+    color: "white",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+  },
+  viewer: {
+    width: window.innerWidth, //the pdf viewer will take up all of the width and height
+    height: window.innerHeight,
+  },
+});
+
+// Create Document Component
+function BasicDocument() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PDFViewer style={styles.viewer}>
+      {/* Start of the document*/}
+      <Document>
+        {/*render a single page*/}
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text>Hello</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>World</Text>
+          </View>
+        </Page>
+      </Document>
+    </PDFViewer>
   );
 }
+export default BasicDocument;
 
-export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
